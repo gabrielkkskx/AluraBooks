aa66706d141963ff648c5141276f3c5a68381435
Gabriel Oliveira <goliveira182826@gmail.com>
2023-11-03 10:21:58 -0300

    tesste

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+ src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"
     const swiper = new Swiper('.swiper', {
         spaceBetween: 10,
         slidesPerView: 3,
