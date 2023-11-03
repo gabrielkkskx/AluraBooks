c0075598bb57d7a7ffc9172a28b5dadf8fb8ba1a
Gabriel Oliveira <goliveira182826@gmail.com>
2023-11-03 10:24:13 -0300

    Script

--- index.js
+++
@@ -1,9 +0,0 @@
- src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"
-    const swiper = new Swiper('.swiper', {
-        spaceBetween: 10,
-        slidesPerView: 3,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-        },
-    });
